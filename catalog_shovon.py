8c4b059380ff1d1052c412410e7bd96c93db3bb6
Akash <tajuar.akash@gmail.com>
2026-09-25 20:34:31 +0600

    fixed catelog printing issue after running main.py

--- catalog_shovon.py
+++ catalog_shovon.py
@@ -14,7 +14,8 @@ def get_available_products(catalog_db):
 def search_products(catalog_db, keyword):
     """Performs a case-insensitive search matching keyword.lower() against product names."""
     return [product for product in catalog_db if keyword.lower() in product["name"].lower()]
-print(search_products(catalog_db, "keyboard"))
+
+# print(search_products(catalog_db, "keyboard"))
 
 def update_stock(catalog_db, product_id, quantity):
     """Decrements stock by quantity for the given product ID upon checkout."""
